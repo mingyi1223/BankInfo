fbeb903544e7d73b1cf4509fff252eb2c5a2eb56
Josh Lin <mingyi1223@gmail.com>
2024-06-26 15:46:07 +0800

    update banks search input

--- banks/views.py
+++ banks/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Banks, Branches
+from django.http import JsonResponse
 
 def index(request):
   banks = Banks.objects.all()
-  return render(request, "banks/index.html", {'banks':banks})
+  bank_list = []
+  for bank in banks:
+    bank_list.append({
+      'code':bank.bank_code,
+      'title':bank.bank
+      })
+  return render(request, "banks/index.html", {'banks':banks, 'bank_list':bank_list})
+
