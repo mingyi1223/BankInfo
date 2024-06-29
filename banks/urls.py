1b86cc45b098d05039b9fd47eb322f797a1fceb2
Josh Lin <mingyi1223@gmail.com>
2024-06-30 03:00:17 +0800

    create urls

--- banks/urls.py
+++ banks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<bank_code>/<branch_code>/<bank_title>-<branch_title>.html', views.index, name='detail'),
 ]
--- banks/views.py
+++ banks/views.py
@@ -1,24 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Banks, Branches
-from django.http import JsonResponse
 
-def index(request):
-  banks = Banks.objects.all()
-  bank_list = []
-  for bank in banks:
-    bank_list.append({
-      'code':bank.bank_code,
-      'title':bank.bank
-      })
-  branches = Branches.objects.all()
-  branch_list = []
-  for branch in branches:
-    branch_list.append({
-      'code':branch.branch_code,
-      'title':branch.branch,
-      'address':branch.address,
-      'tel':branch.tel,
-      'bank_code': branch.head_code.bank_code
-    })
-  return render(request, "banks/index.html", {'banks':banks, 'bank_list':bank_list, 'branch_list':branch_list})
+def index(request, bank_code=None, branch_code=None, bank_title=None, branch_title=None):
+    banks = Banks.objects.all()
+    bank_list = []
+    for bank in banks:
+        bank_list.append({
+            'code': bank.bank_code,
+            'title': bank.bank
+        })
+
+    branches = Branches.objects.all()
+    branch_list = []
+    for branch in branches:
+        branch_list.append({
+            'code': branch.branch_code,
+            'title': branch.branch,
+            'address': branch.address,
+            'tel': branch.tel,
+            'bank_code': branch.head_code.bank_code
+        })
+
+    selected_bank = None
+    selected_branch = None
+
+    if bank_code and branch_code:
+        selected_bank = get_object_or_404(Banks, bank_code=bank_code)
+        selected_branch = get_object_or_404(Branches, branch_code=branch_code, head_code=selected_bank)
+
+    context = {
+        'banks': banks,
+        'bank_list': bank_list,
+        'branch_list': branch_list,
+        'selected_bank': selected_bank,
+        'selected_branch': selected_branch
+    }
+
+    return render(request, "banks/index.html", context)
+
 
