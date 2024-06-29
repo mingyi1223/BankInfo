b8c6a741cca608471a0c80876eb4920df27b713d
Josh Lin <mingyi1223@gmail.com>
2024-06-29 23:07:48 +0800

    update index.html

--- banks/views.py
+++ banks/views.py
@@ -10,5 +10,15 @@ def index(request):
       'code':bank.bank_code,
       'title':bank.bank
       })
-  return render(request, "banks/index.html", {'banks':banks, 'bank_list':bank_list})
+  branches = Branches.objects.all()
+  branch_list = []
+  for branch in branches:
+    branch_list.append({
+      'code':branch.branch_code,
+      'title':branch.branch,
+      'address':branch.address,
+      'tel':branch.tel,
+      'bank_code': branch.head_code.bank_code
+    })
+  return render(request, "banks/index.html", {'banks':banks, 'bank_list':bank_list, 'branch_list':branch_list})
 
